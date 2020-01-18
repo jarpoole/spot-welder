6cf853213b4dd323f5dc884ad01c6ab13b240fda
jaredpooole <jaredpoole123@gmail.com>
2020-01-18 12:00:12 -0500

    all

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,13 +1,32 @@
 //We always have to include the library
 #include "LedControl.h"
+#include <math.h>
 
- //DataIn=11, CLK=13, LOAD=12, #MAX7219 devices
+// DataIn=11, CLK=13, LOAD=12, #MAX7219 devices
 LedControl lc=LedControl(11,13,12,1);
-
-/* we always wait a bit between updates of the display */
+// We always wait a bit between updates of the display
 unsigned long delaytime=250;
 
+//Encoder Stuff
+static int pinA = 2; // Our first hardware interrupt pin is digital pin 2
+static int pinB = 3; // Our second hardware interrupt pin is digital pin 3
+static int pinButton = 4; // Encoder button pin
+volatile byte aFlag = 0; // let's us know when we're expecting a rising edge on pinA to signal that the encoder has arrived at a detent
+volatile byte bFlag = 0; // let's us know when we're expecting a rising edge on pinB to signal that the encoder has arrived at a detent (opposite direction to when aFlag is set)
+volatile uint32_t encoderPos = 0; //this variable stores our current value of encoder position. Change to int or uin16_t instead of byte if you want to record a larger range than 0-255
+volatile uint32_t oldEncPos = 0; //stores the last encoder position value so we can compare to the current reading and see if it has changed (so we know when to print to the serial monitor)
+volatile byte reading = 0; //somewhere to store the direct values we read from our interrupt pins before checking to see if we have moved a whole detent
+volatile bool lastButtonState = false; //Last value of the encoder button
+
 void setup() {
+  pinMode(pinA, INPUT_PULLUP); // set pinA as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+  pinMode(pinB, INPUT_PULLUP); // set pinB as an input, pulled HIGH to the logic voltage (5V or 3.3V for most cases)
+  attachInterrupt(0,PinA,RISING); // set an interrupt on PinA, looking for a rising edge signal and executing the "PinA" Interrupt Service Routine (below)
+  attachInterrupt(1,PinB,RISING); // set an interrupt on PinB, looking for a rising edge signal and executing the "PinB" Interrupt Service Routine (below)
+  Serial.begin(9600); // start the serial monitor link
+
+  pinMode(pinButton, INPUT_PULLUP);
+  
   // The MAX7219 is in power-saving mode on startup, we have to do a wakeup call
   lc.shutdown(0,false);
   // Set the brightness to a medium values
@@ -17,13 +36,44 @@ void setup() {
 }
 void loop() {
   //Device#, digit#, value, decimal
-  lc.setDigit(0,0,5,false);
-  delay(1000);
-  //delay(delaytime);
-  
-  //lc.clearDisplay(0);
-  
-  lc.setChar(0,0,'a',true);
-  delay(1000);
-  //delay(delaytime);
+  //lc.setDigit(0,0,5,false)
+
+  if(oldEncPos != encoderPos) {
+    Serial.println(encoderPos);
+    oldEncPos = encoderPos;
+  }
+  printNum(encoderPos);
+  delay(delaytime);
+}
+
+void PinA(){
+  cli(); //stop interrupts happening before we read pin values
+  reading = PIND & 0xC; // read all eight pin values then strip away all but pinA and pinB's values
+  if(reading == B00001100 && aFlag) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
+    encoderPos --; //decrement the encoder's position count
+    bFlag = 0; //reset flags for the next turn
+    aFlag = 0; //reset flags for the next turn
+  }
+  else if (reading == B00000100) bFlag = 1; //signal that we're expecting pinB to signal the transition to detent from free rotation
+  sei(); //restart interrupts
+}
+void PinB(){
+  cli(); //stop interrupts happening before we read pin values
+  reading = PIND & 0xC; //read all eight pin values then strip away all but pinA and pinB's values
+  if (reading == B00001100 && bFlag) { //check that we have both pins at detent (HIGH) and that we are expecting detent on this pin's rising edge
+    encoderPos ++; //increment the encoder's position count
+    bFlag = 0; //reset flags for the next turn
+    aFlag = 0; //reset flags for the next turn
+  }
+  else if (reading == B00001000) aFlag = 1; //signal that we're expecting pinA to signal the transition to detent from free rotation
+  sei(); //restart interrupts
+}
+
+void printNum(uint32_t value){
+  int c = 0; 
+  while (value != 0){
+    lc.setDigit(0, c, value % 10, false);
+    value /= 10;
+    c++;
+  }
 }
